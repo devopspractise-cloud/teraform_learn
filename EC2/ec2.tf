0055420e3190f01d07d83e399e29f427b9a1515c
Parin <Parinitha@example.com>
2024-09-18 13:44:59 +0530

    first security group pushed

---
+++ EC2/ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_ssh_all" {
+name  =   "allow SSH"
+description =  "to allow SSH post 22"
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ tags = {
+         Name  = "allow SSH-all"
+  }
+
+}
